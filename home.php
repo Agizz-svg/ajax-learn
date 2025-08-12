cd8ff84a998ea10a5ac109265d95e6590fda1295
Agizz-svg <Argies2007>
2025-08-12 11:34:25 +0700

    home commit

---
+++ home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+    <h2>Halaman Depan<h2>
+    <p>Selamat datang di www.malasngoding.com. ini merupakan halaman dengan pada demo cara membuat halaman website ajax tanpa reload dengan ajax jquery</p>
+</div>
